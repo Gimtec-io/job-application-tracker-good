f1b83a3664edb3663afecefc85fae15b19c2fb85
Llorenç <llorenc.muntaner@gmail.com>
2021-10-25 11:40:43 +0200

    Frontend: Responsiveness

--- job-tracker-frontend/src/components/Layout/index.tsx
+++ job-tracker-frontend/src/components/Layout/index.tsx
@@ -1,32 +1,20 @@
 import { Box, Heading } from 'grommet';
 import { ReactNode } from 'react';
-import styled from 'styled-components';
-import { brandColor } from '../../styles/constants';
-
-const Header = styled.header`
-  width: 100%;
-  background-color: ${brandColor};
-`;
-
-const Container = styled.div`
-  width: 75%;
-  margin: 0 auto;
-`;
 
 type Props = {
   children: ReactNode,
 }
 export const Layout = ({ children }: Props) => {
   return (
-    <Box fill>
-      <Header>
-        <Container>
+    <Box align="center">
+      <Box as="header" fill="horizontal" background="brand">
+        <Box width="large" pad="medium">
           <Heading color="white" level="1">Job Tracker</Heading>
-        </Container>
-      </Header>
-      <Container>
+        </Box>
+      </Box>
+      <Box width="large" pad="medium">
         { children }
-      </Container>
+      </Box>
     </Box>
   )
 }
